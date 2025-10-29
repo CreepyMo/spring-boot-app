ef02d27cb550911059c4dbedb68da8f87802c96b
Stas Blashchuk <stanislav.blashchuk@cinarra.com>
2025-10-29 21:36:45 +0100

    primary qualifier practice

--- src/main/java/com/sblashchuk_hub/iam_service/service/impl/SecondCommentServiceImpl.java
+++ src/main/java/com/sblashchuk_hub/iam_service/service/impl/SecondCommentServiceImpl.java
@@ -1,11 +1,13 @@
 package com.sblashchuk_hub.iam_service.service.impl;
 
 import com.sblashchuk_hub.iam_service.service.CommentService;
+import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
+@Service("advancedCommentService")
 public class SecondCommentServiceImpl implements CommentService {
     private final List<String> comments = new ArrayList<>();
 
